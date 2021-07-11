7d38fdd5d4226d597637671a5f51a70ffa496644
WhiteStan <stas147741@gmail.com>
2021-07-11 21:12:40 +0300

    Minor style fix and README update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FirstTouchOnKtl
+
+As a first step I created an Spring MVC application with JOOQ for database layer.
+Once CRUD would be functional on this step, I'll move with updating to WebFlux. 
--- src/main/kotlin/com/firsttouch/ktl/KtlApplication.kt
+++ src/main/kotlin/com/firsttouch/ktl/KtlApplication.kt
@@ -1,17 +1,12 @@
 package com.firsttouch.ktl
 
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
-import org.springframework.boot.autoconfigure.jooq.JooqAutoConfiguration
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication(exclude = [R2dbcAutoConfiguration::class])
 @EnableTransactionManagement
-@ImportAutoConfiguration(JooqAutoConfiguration::class)
 class KtlApplication
 
 fun main(args: Array<String>) {
