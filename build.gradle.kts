dc7347602c30b166605bc3da0b31bc09202d2e07
WhiteStan <stas147741@gmail.com>
2021-07-11 14:52:38 +0300

    Added jooq generation script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,23 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import nu.studer.gradle.jooq.JooqEdition
 
 plugins {
 	id("org.springframework.boot") version "2.5.2"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.5.20"
 	kotlin("plugin.spring") version "1.5.20"
+	id("nu.studer.jooq") version "6.0"
 }
 
 group = "com.firsttouch"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
+
 repositories {
 	mavenCentral()
 }
+apply(plugin = "nu.studer.jooq")
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-jooq")
@@ -23,8 +27,9 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	runtimeOnly("mysql:mysql-connector-java")
+	jooqGenerator("mysql:mysql-connector-java:8.0.11")
 }
 
 tasks.withType<KotlinCompile> {
@@ -36,4 +41,40 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+jooq {
+	version.set("3.15.0")
+	edition.set(JooqEdition.OSS)
+
+	configurations {
+		create("main") {
+			jooqConfiguration.apply {
+				logging = org.jooq.meta.jaxb.Logging.WARN
+				jdbc.apply {
+					driver = "com.mysql.cj.jdbc.Driver"
+					url = "jdbc:mysql://localhost:3306/app?useSSL=false"
+					user = "root"
+					password = "root"
+				}
+				generator.apply {
+					name = "org.jooq.codegen.DefaultGenerator"
+					database.apply {
+						inputSchema = "app"
+					}
+					generate.apply {
+						isDeprecated = false
+						isRecords = false
+						isImmutablePojos = false
+						isFluentSetters = false
+					}
+					target.apply {
+						packageName = "generated.com.firsttouch.ktl"
+						directory = "src/main/kotlin/"
+					}
+					strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+				}
+			}
+		}
+	}
 }
