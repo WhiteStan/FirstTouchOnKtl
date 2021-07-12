960c6322fe98b0b8324b63a35f87f06b5ddd5ba1
WhiteStan <stas147741@gmail.com>
2021-07-12 21:54:58 +0300

    Prepared template for integration tests

--- src/test/kotlin/com/firsttouch/ktl/KtlApplicationTests.kt
+++ src/test/kotlin/com/firsttouch/ktl/KtlApplicationTests.kt
@@ -1,10 +1,24 @@
 package com.firsttouch.ktl
 
+import com.firsttouch.ktl.repository.CitizenRepository
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class KtlApplicationTests {
+
+	@Autowired
+	lateinit var client: TestRestTemplate
+
+	@Autowired
+	lateinit var citizenRepository: CitizenRepository
+
+	@BeforeEach
+	fun setUp() {
+	}
 
 	@Test
 	fun contextLoads() {
